222e45c50fc06cd8ce931e5d2ae12f97405f5874
Ian Boyle <irboyle21@gmail.com>
2021-11-12 16:11:39 -0500

    adds index and show routes for stocks

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,2 +1,10 @@
 class StocksController < ApplicationController
+  def index
+    stocks = Stock.all
+    render json: stocks.as_json
+  end
+  def show
+    stock = Stock.find_by(id: params[:id])
+    render json: stock.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+
+  ###STOCKS ROUTES###
+  get "/stocks" => "stocks#index"
+  get "/stocks/:id" => "stocks#show"
+
   ###USER ROUTES###
   post "/users" => "users#create"
 
