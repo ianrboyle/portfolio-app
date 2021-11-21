fcd40f7a13f45631ca9e9431f50692ecef847609
Ian Boyle <irboyle21@gmail.com>
2021-11-21 10:57:17 -0500

    adds index, show and create actions for sector

--- app/controllers/sectors_controller.rb
+++ app/controllers/sectors_controller.rb
@@ -1,11 +1,23 @@
 class SectorsController < ApplicationController
   def index
     sectors = Sector.all 
-    render json: sectors.as_json
+    render json: sectors
   end
 
   def show
     sector = Sector.find_by(id: params[:id])
-    render json: sector.as_json
+    render json: sector
+  end
+
+  def create
+    sector = Sector.new(
+      sector: params[:sector]
+    )
+    sector.save
+    if sector.save
+      render json: sector
+    else
+      render json: {errors: sector.errors.full_messages}, status: 406
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,4 +2,8 @@ class Stock < ApplicationRecord
   belongs_to :user
   belongs_to :sector
   belongs_to :industry
+
+  def current_total_value
+    (quantity * current_price).round(3)
+  end
 end
