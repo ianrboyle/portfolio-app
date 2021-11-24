28ca61fe65bd0bcf52610b02dea2e188f19adc22
Ian Boyle <irboyle21@gmail.com>
2021-11-24 16:22:40 -0500

    adds sectors and stocks to industry serializer

--- app/serializers/industry_serializer.rb
+++ app/serializers/industry_serializer.rb
@@ -1,3 +1,3 @@
 class IndustrySerializer < ActiveModel::Serializer
-  attributes :id, :industry, :industry_percent_of_account
+  attributes :id, :industry, :industry_percent_of_account, :sectors, :stocks
 end
