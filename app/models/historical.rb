0d99ca3620082bc877512fbfe8800ebbe33cae04
Ian Boyle <irboyle21@gmail.com>
2021-12-19 20:59:45 -0500

    adds historical methods - day/gain month/gain

--- app/models/historical.rb
+++ app/models/historical.rb
@@ -1,2 +1,36 @@
 class Historical < ApplicationRecord
+  def day_gain_loss
+    yesterday_date = date-1
+    
+    yesterday = Historical.find_by(date: yesterday_date)
+    if yesterday
+      (portfolio_value - yesterday.portfolio_value).round(3) 
+    else
+      return "no data"
+    end
+  end
+
+  def day_gain_loss_percent
+    yesterday_date = date-1
+    yesterday = Historical.find_by(date: yesterday_date)
+    if yesterday
+      ((day_gain_loss/portfolio_value)*100).round(2)
+    end
+  end
+
+  def month_gain_loss
+    last_month_date = date - 30
+    last_month = Historical.find_by(date: last_month_date)
+    if last_month
+      (portfolio_value - last_month.portfolio_value).round(3)
+    end
+  end
+
+  def month_gain_loss_percent
+    last_month_date = date - 30
+    last_month = Historical.find_by(date: last_month_date)
+    if last_month
+     (month_gain_loss/portfolio_value * 100).round(2)
+    end
+  end
 end
--- app/serializers/historical_serializer.rb
+++ app/serializers/historical_serializer.rb
@@ -1,3 +1,3 @@
 class HistoricalSerializer < ActiveModel::Serializer
-  attributes :id, :portfolio_value, :date
+  attributes :id, :portfolio_value, :date, :day_gain_loss, :day_gain_loss_percent, :month_gain_loss, :month_gain_loss_percent
 end
