8ccac9fbf48cf02a1ff997ffe5e7ccb51e2376da
Ian Boyle <irboyle21@gmail.com>
2023-12-02 09:01:55 -0700

    adds signup user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,12 +2,11 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
-import { CreateUserDto } from './dtos/create-user-dto';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
-  create(userDto: CreateUserDto, email: string, password: string) {
+  create(email: string, password: string) {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
   }
